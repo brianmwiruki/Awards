d5c50870227b8fbeede21f712dfaf35d4bc7d47a
Brian Mwiruki <mwiruki@outlook.com>
2020-03-22 13:28:38 +0300

    django upgrade to django 2.0.1

--- myawards/settings.py
+++ myawards/settings.py
@@ -40,6 +40,8 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
 INSTALLED_APPS = [
     'star_ratings',
     'awards.apps.AwardsConfig',
