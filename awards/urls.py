b226cf77ae93c8dc8200d7563e540bb6ce0f136b
Brian Mwiruki <mwiruki@outlook.com>
2020-03-23 09:03:38 +0300

    star_ratings.url diasble for debug purposes

--- awards/urls.py
+++ awards/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+# from rest_framework import routers, serializers, viewsets
+# from django.contrib.auth.models import User
 
 urlpatterns = [
     url(r'^$', views.home, name= 'home'),
